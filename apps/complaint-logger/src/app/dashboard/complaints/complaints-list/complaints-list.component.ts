5f206e245b82b048be9c0ac92b3f85b5170d31d9
Rohit Chopra <rohitchopra@Rohits-Air.fritz.box>
2019-08-17 23:24:00 +0530

    Complaint List(UI): Complaint List pending+resolved

--- apps/complaint-logger/src/app/dashboard/complaints/complaints-list/complaints-list.component.ts
+++ apps/complaint-logger/src/app/dashboard/complaints/complaints-list/complaints-list.component.ts
@@ -3,7 +3,9 @@ import { ComplaintsListService } from './complaints-list.service';
 import { BehaviorSubject, Observable, combineLatest, Subject } from 'rxjs';
 import { Complaint, ComplaintStatus } from '@complaint-logger/models';
 import { switchMap, map, shareReplay, filter } from 'rxjs/operators';
+import { PageEvent } from '@angular/material';
 
+import * as moment from 'moment';
 @Component({
   selector: 'complaint-logger-complaints-list',
   templateUrl: './complaints-list.component.html',
@@ -13,34 +15,51 @@ import { switchMap, map, shareReplay, filter } from 'rxjs/operators';
 export class ComplaintsListComponent implements OnInit {
   ComplaintStatus = ComplaintStatus;
   tabChangeEvent = new BehaviorSubject<number>(1);
-  private _pendingPagination = {
-    pageSize: 10,
+  pendingPagination = new BehaviorSubject({
+    pageSize: 5,
     pageNumber: 1
-  };
-  private _resolvedPagination = {
-    pageSize: 10,
+  });
+  resolvedPagination = new BehaviorSubject({
+    pageSize: 5,
     pageNumber: 1
-  };
-  pendingPagination = new BehaviorSubject(this._pendingPagination);
-  resolvedPagination = new BehaviorSubject(this._resolvedPagination);
+  });
   pending$ = combineLatest(this.tabChangeEvent, this.pendingPagination).pipe(
     filter(([selectedTabIndex, _]) => selectedTabIndex === 1),
     switchMap(([_, { pageSize, pageNumber }]) => this.dataService.complaints({ pageSize, pageNumber, status: ComplaintStatus.Pending })),
+    map(c => {
+      c.complaints.forEach(complaint => Object.assign(complaint, { createdAt: moment(complaint.createdAt).fromNow() }))
+      return c;
+    }),
     shareReplay()
   );
   resolved$ = combineLatest(this.tabChangeEvent, this.resolvedPagination).pipe(
     filter(([selectedTabIndex, _]) => selectedTabIndex === 2),
     switchMap(([_, { pageSize, pageNumber }]) => this.dataService.complaints({ pageSize, pageNumber, status: ComplaintStatus.Resolved })),
+    map(c => {
+      c.complaints.forEach(complaint => Object.assign(complaint, { createdAt: moment(complaint.createdAt).fromNow() }))
+      return c;
+    }),
     shareReplay()
   );
 
   constructor(private readonly dataService: ComplaintsListService) {
-
     this.tabChangeEvent.next(1);
   }
 
   ngOnInit() {
+  }
 
+  pendingPageChanged(ev: PageEvent) {
+    this.pendingPagination.next({
+      pageNumber: ev.pageIndex + 1,
+      pageSize: ev.pageSize
+    });
+  }
+  resolvedPageChanged(ev: PageEvent) {
+    this.resolvedPagination.next({
+      pageNumber: ev.pageIndex + 1,
+      pageSize: ev.pageSize
+    });
   }
 }
 
