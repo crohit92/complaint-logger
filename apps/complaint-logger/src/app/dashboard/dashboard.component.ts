d680fc55deb9acbf7157b536ff356857156ce770
Rohit Chopra <rohitchopra@Rohits-Air.fritz.box>
2019-09-14 23:29:04 +0530

    Refactor: Welcome logged in user

--- apps/complaint-logger/src/app/dashboard/dashboard.component.ts
+++ apps/complaint-logger/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { StorageService } from '../core/services/storage/storage.service';
 import { Router } from '@angular/router';
+import { StorageKeys } from '../shared/constants/storage-keys';
+import { UserTypes, User } from '@complaint-logger/models';
 
 @Component({
   selector: 'complaint-logger-dashboard',
@@ -8,11 +10,24 @@ import { Router } from '@angular/router';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
+  userType: string;
+  user: User;
   constructor(
     private readonly storage: StorageService,
     private readonly router: Router
-  ) { }
+  ) {
+    this.user = this.storage.get(StorageKeys.user) as User;
+    switch (this.user.type) {
+      case UserTypes.Student:
+        this.userType = 'Student';
+        break;
+      case UserTypes.Department:
+        this.userType = 'Department';
+        break;
+      case UserTypes.Employee:
+        this.userType = 'Employee';
+    }
+  }
 
   ngOnInit() {
   }
