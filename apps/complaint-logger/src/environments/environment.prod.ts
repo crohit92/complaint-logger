97bee41d0696f394acc838100044dfb1045d7c05
Rohit Chopra <crohit92@gmail.com>
2020-02-26 08:40:35 +0530

    App uploaded to playstore

--- apps/complaint-logger/src/environments/environment.prod.ts
+++ apps/complaint-logger/src/environments/environment.prod.ts
@@ -3,4 +3,6 @@ export const environment = {
   apiBase: '/api/v1',
   reopenWindow: 86400000,
   adminCanCloseAfter: 86400000
+  // apiBase: 'http://172.105.57.8/api/v1',
+  // android:usesCleartextTraffic="true"
 };
